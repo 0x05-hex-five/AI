2918b1875613713adcaa9a04a91149aeeea5dc70
seungwoo900 <daniel3633@sju.ac.kr>
2025-05-07 22:20:09 +0900

    feat: implement dataloaders for cross-validation

--- clf/src/data.py
+++ clf/src/data.py
@@ -1,20 +1,37 @@
 import os, cv2, numpy as np
 from pathlib import Path
-from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler, Subset
 from torchvision.datasets import ImageFolder
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
+from sklearn.model_selection import StratifiedKFold
 
-root = Path("./dataset/split")
-
-tmp_ds = ImageFolder(root / "train")
-counts = np.bincount(tmp_ds.targets) # Count the number of images in each class
-
-threshold = counts.mean() # Set threshold to the mean of images in each class
-print("threshold: ", threshold)
-under_cls = {i for i, c in enumerate(counts) if c < threshold} # Classes with less than threshold images
-# print("under_cls", [tmp_ds.classes[i] for i in under_cls])
-print("num under-classes:", len(under_cls))
+def find_under_represented_classes(root, threshold=None):
+    ds = ImageFolder(root)
+    counts = np.bincount(ds.targets)
+    if threshold is None:
+        threshold = counts.mean()
+    under = {i for i, c in enumerate(counts) if c < threshold}
+    return under
+    
+def get_transforms(img_size=300):
+    base_tf = A.Compose([
+        A.Resize(img_size, img_size),
+        A.Normalize(),
+        ToTensorV2(),
+    ])
+    heavy_tf = A.Compose([
+        A.Resize(img_size, img_size),
+        A.ShiftScaleRotate(
+            shift_limit=0.0625, scale_limit=0.1, rotate_limit=15,
+            border_mode=cv2.BORDER_CONSTANT, p=0.5
+        ),
+        A.RandomBrightnessContrast(brightness_limit=0.1,
+                                   contrast_limit=0.1, p=1.0),
+        A.Normalize(),
+        ToTensorV2(),
+    ])
+    return base_tf, heavy_tf
 
 class CustomAugDataset(Dataset):
     def __init__(self, root, base_tf, heavy_tf, under_cls):
@@ -33,63 +50,115 @@ class CustomAugDataset(Dataset):
         tf = self.heavy_tf if target in self.under_cls else self.base_tf
         out = tf(image=imp_np)["image"]
         return out, target
-    
-base_tf = A.Compose([
-    A.Resize(300, 300), # Resize to 300x300
-    A.Normalize(), # Normalize the image
-    ToTensorV2(), # Convert to tensor
-])
-heavy_tf = A.Compose([
-    A.Resize(300, 300), # Resize to 300x300
-    A.ShiftScaleRotate(shift_limit=0.0625,   # location
-                       scale_limit=0.1,      # scale
-                       rotate_limit=15,      # rotation
-                       border_mode=cv2.BORDER_CONSTANT,
-                       p=0.5), # Randomly shift, scale, and rotate the image
-    A.RandomBrightnessContrast(brightness_limit=0.1, contrast_limit=0.1, p=1.0), # Brightness and contrast
-    A.Normalize(), # Normalize the image
-    ToTensorV2(), # Convert to tensor
-])
-
-train_ds = CustomAugDataset(root / "train", base_tf, heavy_tf, under_cls) # solve class imbalance with augmentation
-val_ds = ImageFolder(root / "val", transform=base_tf)
-test_ds = ImageFolder(root / "test", transform=base_tf)
 
 # Weights for the WeightedRandomSampler
-counts = np.bincount(train_ds.dataset.targets) # Count the number of images in each class
-class_weights = 1.0 / counts.astype(float)
-sample_weights = class_weights[train_ds.dataset.targets] # Weights for each sample
-sampler = WeightedRandomSampler(
-    weights=sample_weights,
-    num_samples=len(train_ds.dataset),
-    replacement=True, # Allow duplicate samples
-)
+def make_sampler(ds):
+    if isinstance(ds, Subset): # If using a subset, get the base dataset and indices
+        base_ds = ds.dataset
+        idxs = ds.indices
+    else:
+        base_ds = ds # CustomAugDataset
+        idxs = list(np.arange(len(ds)))
 
-train_loader = DataLoader(
-    train_ds,
-    batch_size=64,
-    sampler=sampler, # Use the sampler for class imbalance
-    num_workers=0,
-    pin_memory=True,
-    shuffle=False,
-    drop_last=True,
-)
+    labels = base_ds.dataset.targets # ImageFolder labels
+    counts = np.bincount(labels) # Count the number of images in each class
+    class_weights = 1.0 / counts.astype(float)
+    sample_weights = [class_weights[labels[i]] for i in idxs] # Weights for each sample
+    return WeightedRandomSampler(sample_weights,
+                                 num_samples=len(sample_weights),
+                                 replacement=True ) # Allow duplicate samples
 
-val_loader = DataLoader(
-    val_ds,
-    batch_size=64,
-    num_workers=0,
-    pin_memory=True,
-    shuffle=False,
-)
+def alb_to_tensor(img, tf):
+    img = np.array(img)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR) # Convert to BGR for albumentations
+    img = tf(image=img)["image"] # Apply the transformation
+    return img
 
-test_loader = DataLoader(
-    test_ds,
-    batch_size=64,
-    num_workers=0,
-    pin_memory=True,
-    shuffle=False,
-)
+def make_dataloaders(cfg):
+    root = Path(cfg["data"]["root"])
+    img_size = cfg["data"].get("img_size", 300)
+    base_tf, heavy_tf = get_transforms(img_size)
 
-print(len(train_loader.dataset), len(val_loader.dataset), len(test_loader.dataset))
-print(len(train_loader), len(val_loader), len(test_loader))
+    under = find_under_represented_classes(root/"train",
+                                           threshold=cfg["data"].get("threshold"))
+
+    train_ds = CustomAugDataset(root/"train", base_tf, heavy_tf, under)
+    val_ds   = ImageFolder(root/"val", transform=lambda x: alb_to_tensor(x, base_tf))
+    test_ds  = ImageFolder(root/"test",  transform=lambda x: alb_to_tensor(x, base_tf))
+
+    sampler = make_sampler(train_ds)
+
+    train_loader = DataLoader(train_ds,
+                              batch_size=cfg["train"]["batch_size"],
+                              sampler=sampler, # Use the sampler for class imbalance
+                              num_workers=cfg["train"]["num_workers"],
+                              pin_memory=True, # Pin memory for faster data transfer to GPU
+                              drop_last=True
+                              )
+    val_loader = DataLoader(val_ds,
+                            batch_size=cfg["train"]["batch_size"],
+                            shuffle=False,
+                            num_workers=cfg["train"]["num_workers"]//4,
+                            pin_memory=True
+                            )
+    test_loader = DataLoader(test_ds,
+                             batch_size=cfg["train"]["batch_size"],
+                             shuffle=False,
+                             num_workers=cfg["train"]["num_workers"]//4,
+                             pin_memory=True
+                             )
+    return train_loader, val_loader, test_loader
+
+# Function for cross-validation
+def make_cv_dataloaders(cfg, fold: int, n_splits: int = 5):
+    root = Path(cfg["data"]["root"])
+    img_size = cfg["data"].get("img_size", 300)
+    base_tf, heavy_tf = get_transforms(img_size)
+
+    # The whole train ImageFolder
+    full = CustomAugDataset(root/"train", base_tf, heavy_tf, find_under_represented_classes(root/"train"))
+
+    # The base ImageFolder for validation
+    val_base = ImageFolder(root/"train", transform=lambda img: alb_to_tensor(img, base_tf))
+
+    # Stratified K-Folds cross-validation
+    skf = StratifiedKFold(n_splits=n_splits, shuffle=True, random_state=42)
+    splits = list(skf.split(np.zeros(len(full)), full.dataset.targets))
+    train_idx, val_idx = splits[fold]
+
+    train_ds = Subset(full, train_idx)
+    val_ds   = Subset(val_base, val_idx) # Use the original dataset for validation
+
+    sampler = make_sampler(train_ds)
+
+    train_loader = DataLoader(train_ds,
+                              batch_size=cfg["train"]["batch_size"],
+                              sampler=sampler, # Use the sampler for class imbalance
+                              num_workers=cfg["train"]["num_workers"],
+                              pin_memory=True, # Pin memory for faster data transfer to GPU
+                              drop_last=True
+                              )
+    val_loader = DataLoader(val_ds,
+                            batch_size=cfg["train"]["batch_size"],
+                            shuffle=False,
+                            num_workers=cfg["train"]["num_workers"]//4,
+                            pin_memory=True
+                            )
+    
+    return train_loader, val_loader
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    import yaml
+
+    p = ArgumentParser()
+    p.add_argument("--config", default="default.yaml")
+    args = p.parse_args()
+
+    cfg = yaml.safe_load(open(args.config, "r"))
+    # tl, vl, tsl = make_dataloaders(cfg)
+    # print("datasets:", len(tl.dataset), len(vl.dataset), len(tsl.dataset))
+    # print("batches :", len(tl), len(vl), len(tsl))
+
+    tl, vl = make_cv_dataloaders(cfg, fold=0, n_splits=5)
+    print("datasets:", len(tl.dataset), len(vl.dataset))
